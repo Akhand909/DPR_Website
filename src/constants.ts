8f9ceea3564f1fb024de31f84c8b7e2af8ab1c4d
Akhand909 <akhand.singh@shivit.com>
2026-03-23 13:49:02 +0530

    Initial commit to GitHub

---
+++ src/constants.ts
@@ -0,0 +1,55 @@
+export const COLORS = {
+  primary: '#003366', // Deep Blue
+  secondary: '#E5E7EB', // Steel Grey
+  accent: '#DC2626', // Subtle Red
+  white: '#FFFFFF',
+  text: '#1F2937',
+};
+
+export const IMAGES = {
+  logo: 'https://lh3.googleusercontent.com/d/1SWqivwG2Pgmk7pjOwEPocbjZpaRsDINt',
+  banner1: 'https://lh3.googleusercontent.com/d/1fQ8K72eYR21Y2ua6hiUNsJsgBeK0uset',
+  banner2: 'https://lh3.googleusercontent.com/d/1IIPCAwMSMEpYZTC8Fqfzn74Cd1p_TdCy',
+  banner3: 'https://lh3.googleusercontent.com/d/119D01s_7PrTl_MM5MX49LFyzrr2A-sTu',
+  banner4: 'https://lh3.googleusercontent.com/d/1GyEpttDYD7W45ngqAknaZwz7nQXZjZmc',
+  banner5: 'https://lh3.googleusercontent.com/d/1IprISUvU-YzYbnpeeaI-KEIixp41669O',
+  // Reusing these for other sections as requested
+  awards: 'https://picsum.photos/seed/awards/1200/800',
+  tech: 'https://picsum.photos/seed/tech/1200/800',
+  services: 'https://picsum.photos/seed/services/1200/800',
+  sustainability: 'https://picsum.photos/seed/sustainability/1200/800',
+  partners: 'https://picsum.photos/seed/partners/1200/800',
+  // Indian Infrastructure Projects
+  india_metro: 'https://lh3.googleusercontent.com/d/1GyEpttDYD7W45ngqAknaZwz7nQXZjZmc',
+  india_highway: 'https://picsum.photos/seed/indian-highway/800/600',
+  india_smartcity: 'https://lh3.googleusercontent.com/d/1jS2MFvxs37MSfXs1je_v9kU-c-h0pKzi',
+  navi_mumbai: 'https://lh3.googleusercontent.com/d/1b0yvUk739LpYtAS3_Xo2Lx947YJQlSOl',
+  kovvada_nuclear: 'https://lh3.googleusercontent.com/d/1CL4Zz_sQrz3MrmAVEVbhFCwzb1xwSKpj',
+  jamnagar_refinery: 'https://lh3.googleusercontent.com/d/1VdfL_J69Z_vjteHiEyRGCuktKUYPkF03',
+  india_industrial: 'https://lh3.googleusercontent.com/d/1f2mxYEDLRqHm3xyEu9-ckO0a1M0il6N8',
+  about_image: 'https://lh3.googleusercontent.com/d/1D1C7Ue8gdzVJLl_fvWVnC89ZzfvR0p8S',
+};
+
+export const NAV_LINKS = [
+  {
+    title: 'About Us',
+    submenu: [
+      { title: 'History', href: '/about/history' },
+      { title: 'Accomplishments', href: '/about/accomplishments' },
+      { title: 'Leadership', href: '/about/leadership' },
+      { title: 'Global Presence', href: '/about/global-presence' },
+    ],
+  },
+  {
+    title: 'Business',
+    submenu: [
+      { title: 'Health', href: '/what-we-build/healthcare' },
+      { title: 'Hospitality', href: '/what-we-build/hospitality' },
+      { title: 'Manufacturing', href: '/what-we-build/manufacturing' },
+      { title: 'Mission Critical', href: '/what-we-build/mission-critical' },
+      { title: 'Mining', href: '/what-we-build/mining' },
+    ],
+  },
+  { title: 'Careers', href: '/careers' },
+  { title: 'Contact Us', href: '/contact' },
+];
